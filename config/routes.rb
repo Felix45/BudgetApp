b4e862c2b235cf65dd49d7337ea3de5f9ae896f5
Felix Ouma <fatonoh@gmail.com>
2022-10-12 23:26:33 +0300

    Add landing page to the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'landings#index'
   devise_for :users
   resources :group_bills
   resources :bills
   resources :groups
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
