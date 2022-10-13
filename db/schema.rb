5662c5b7074a1ca64869c4f1f6fe11df92c1a827
Felix Ouma <fatonoh@gmail.com>
2022-10-13 12:49:42 +0300

    Update database schema to include group_id in bills

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_214721) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_000406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_214721) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.bigint "group_id", null: false
     t.index ["author_id"], name: "index_bills_on_author_id"
+    t.index ["group_id"], name: "index_bills_on_group_id"
   end
 
   create_table "group_bills", force: :cascade do |t|
@@ -57,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_214721) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bills", "groups"
   add_foreign_key "bills", "users", column: "author_id"
   add_foreign_key "group_bills", "bills"
   add_foreign_key "group_bills", "groups"
