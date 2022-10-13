cf205fee8f3c8b5762ec62b6c5e1adc61ae9c3b0
Felix Ouma <fatonoh@gmail.com>
2022-10-13 12:48:06 +0300

    Update application routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'landings#index'
   devise_for :users
-  resources :group_bills
-  resources :bills
-  resources :groups
+  resources :groups do
+    resources :bills
+  end
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
