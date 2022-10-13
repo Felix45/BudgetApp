aa972c0fe8f47045d7e35da6cfbc25d5f2b0b931
Felix Ouma <fatonoh@gmail.com>
2022-10-13 13:09:45 +0300

    Fix linter errors using rubocop

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,5 +1,5 @@
 class LandingsController < ApplicationController
-  before_action :authenticate_user! , :except => [ :index ]
+  before_action :authenticate_user!, except: [:index]
   def index
     @title = 'HelaPal'
   end
