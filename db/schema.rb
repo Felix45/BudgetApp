10b697f10fa663a1a8a79502f29455359b37e8f9
Felix Ouma <fatonoh@gmail.com>
2022-10-14 21:18:14 +0300

    Update application schema to include description on bills table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_000406) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_140808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_000406) do
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
     t.bigint "group_id", null: false
+    t.string "description"
     t.index ["author_id"], name: "index_bills_on_author_id"
     t.index ["group_id"], name: "index_bills_on_group_id"
   end
