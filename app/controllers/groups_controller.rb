c528913be7e5440f978c2b6db7e7b68c457f5a78
Felix Ouma <fatonoh@gmail.com>
2022-10-14 13:28:56 +0300

    Fetch groups belonging to a logged in user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,11 +3,15 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = current_user.groups.includes(:bills).all.order('id DESC')
+    @title = 'Transactions'
   end
 
   # GET /groups/1 or /groups/1.json
-  def show; end
+  def show
+    @group = current_user.groups.includes(:bills).order('id DESC').find(params[:id])
+    @title = @group.name
+  end
 
   # GET /groups/new
   def new
@@ -23,7 +27,7 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully created.' }
+        format.html { redirect_to groups_url, notice: 'Group was successfully created.' }
         format.json { render :show, status: :created, location: @group }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -36,7 +40,7 @@ class GroupsController < ApplicationController
   def update
     respond_to do |format|
       if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully updated.' }
+        format.html { redirect_to groups_url, notice: 'Group was successfully updated.' }
         format.json { render :show, status: :ok, location: @group }
       else
         format.html { render :edit, status: :unprocessable_entity }
