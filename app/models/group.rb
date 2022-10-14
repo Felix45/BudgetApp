e147e464e3a429d853aae626187446c28997d93b
Felix Ouma <fatonoh@gmail.com>
2022-10-15 01:30:07 +0300

    Add group model unit tests

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   mount_uploader :icon, IconUploader
   has_many :bills, dependent: :destroy
   belongs_to :user
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
